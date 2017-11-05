032736ebbc131e3640e5ff95c3d9802893e26381
seungeun kim <>
2017-11-05 11:52:30 +0900

    fix http routes

--- src/main/scala/com/restapi/TwitterWordsApplication.scala
+++ src/main/scala/com/restapi/TwitterWordsApplication.scala
@@ -1,24 +1,10 @@
 package com.restapi
 
-import akka.event.Logging
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Directives
 import com.restapi.routes.WordsHttpRoute
-import com.restapi.services.WordsServiceImpl
 
-import scala.util.Failure
+object TwitterWordsApplication extends App {
 
-object TwitterWordsApplication extends App with WordsServiceImpl with Directives with JsonSupport {
-  private val logger = Logging(system, getClass)
-
-  val interface = "localhost"
-  val port = 8080
-
-  val binding = Http().bindAndHandle(WordsHttpRoute.route, interface, port)
-  binding.onComplete {
-    case scala.util.Success(s) => logger.info("Success")
-    case Failure(f) => logger.error(f, s"Failed to bind to $interface $port")
-  }
+  WordsHttpRoute.startApplication
 
 }
 
